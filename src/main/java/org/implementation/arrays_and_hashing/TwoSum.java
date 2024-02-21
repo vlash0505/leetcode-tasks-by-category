38df53b3face20a6cde23d2ac9d32ed9a70abb87
vlash <shupliakovvl@gmail.com>
2024-02-21 22:09:07 +0200

    [arrays] - 1. TwoSum //update task description

--- src/main/java/org/implementation/arrays_and_hashing/TwoSum.java
+++ src/main/java/org/implementation/arrays_and_hashing/TwoSum.java
@@ -1,7 +1,10 @@
 package org.implementation.arrays_and_hashing;
 
 /*
-* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+1. Two Sum
+Easy
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
